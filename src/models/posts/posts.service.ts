1158782646b9ec4b097844f17243eaedcb9d4dc1
alejandro <lopez.alejandro@dali.dartmouth.edu>
2023-11-15 20:35:43 -0500

    added mongo

--- src/models/posts/posts.service.ts
+++ src/models/posts/posts.service.ts
@@ -1,11 +1,16 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
-import { Post } from './interfaces/posts.interface';
+// import { Post } from './interfaces/posts.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Post } from './schemas/post.schema';
 
 @Injectable()
 export class PostsService {
+  constructor(@InjectModel(Post.name) private postModel: Model<Post>) {}
+
   private readonly posts: Post[] = [];
 
-  getPosts(): Post[] {
-    return this.posts;
+  async getPosts(): Promise<Post[]> {
+    return this.postModel.find().exec();
   }
 }
---
+++ src/models/posts/schemas/post.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type PostDocument = HydratedDocument<Post>;
+
+@Schema()
+export class Post {
+  @Prop()
+  title: string;
+
+  @Prop()
+  date: string;
+
+  @Prop()
+  author: string;
+
+  @Prop()
+  description: string;
+}
+
+export const PostSchema = SchemaFactory.createForClass(Post);
