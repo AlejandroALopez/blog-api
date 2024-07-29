954ef3010db47cc3570c6dda164b13a8b0f532b3
alejandro <lopez.alejandro@dali.dartmouth.edu>
2024-07-29 10:58:55 -0400

    moved deploy from heroku to vercel

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    // origin: 'http://localhost:4200', // for local testing
-    origin: 'https://www.alejandroalopez.com',
+    origin: process.env.CLIENT_URL,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   };
